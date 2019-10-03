801230d639915e57f110d0611ac040cd2b01a198
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2019-10-02 21:42:09 -0300

    first project

---
+++ aula01/aula01/src/aula01/Aula01.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula01;
+
+/**
+ *
+ * @author Lab
+ */
+public class Aula01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        
+        // definição de variáveis
+        int var = 1;
+        var = 100;
+        
+        String vars = "Hello!";
+        
+        // tipos primitivos x classes de tipos
+        // int, float, double / String, Integer, Float
+        
+        int valorInteiro = 0;
+        // variável de 8 bits
+        byte valorByte = 0;
+        float valorFloat = 0f;
+        // dobro do float
+        double valorDouble = 0f;
+        // dobro do inteiro
+        long valorLong = 0;
+        boolean valorBoolean = false;
+        String valorString = "Thalyson!";
+        
+        // operações com inteiros
+        
+        valorInteiro = 100;
+        valorInteiro = 100 * 2;
+        valorInteiro = 100 / 2;
+        valorInteiro = 100 % 3;
+        valorInteiro = 100 + 2;
+        valorInteiro = 100 - 2;
+        
+        // operações de incremento e decremento
+        valorInteiro++;
+        valorInteiro--;
+        ++valorInteiro;
+        --valorInteiro;
+        
+    }
+    
+}
