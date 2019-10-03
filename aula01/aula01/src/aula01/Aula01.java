9672b56e3ee2ba40d8396b5103158b5cb8d7f0cc
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2019-10-02 22:34:50 -0300

    update

--- aula01/aula01/src/aula01/Aula01.java
+++ aula01/aula01/src/aula01/Aula01.java
@@ -52,6 +52,91 @@ public class Aula01 {
         ++valorInteiro;
         --valorInteiro;
         
+        // Operações bitwise - bit a bit
+        valorInteiro = 1024;
+        valorInteiro = valorInteiro | 1;
+        valorInteiro |= 1;
+        valorInteiro = valorInteiro & 1;
+        valorInteiro &= 1;
+        // shift right
+        valorInteiro = valorInteiro >> 2;
+        // shift left
+        valorInteiro = valorInteiro << 1;
+        valorInteiro = valorInteiro ^ 1;
+        valorInteiro = ~ 1;
+        
+        // Operações com Float
+        valorFloat = 100 * .999f;
+        valorFloat = Math.round(valorFloat);
+        
+        // Operações com Double
+        valorDouble = 5;
+        valorDouble = Math.pow(2, 5); // 2 elevado a 5
+        valorDouble = Math.min(5, 50);
+        valorDouble = Math.max(5, 50);
+        valorDouble = Math.sqrt(81);
+        valorDouble = Math.round(81.85);
+        
+        // Operações lógicas
+        valorBoolean = true;
+        valorBoolean = false;
+        valorBoolean = 100 == 10;
+        valorBoolean = 100 != 10;
+        valorBoolean = 100 > 10;
+        valorBoolean = 100 >= 10;
+        valorBoolean = 100 < 10;
+        valorBoolean = 100 <= 10;
+        valorBoolean = (100 >= 10) && (10 < 20);
+        valorBoolean = (100 >= 10) || (10 < 20);
+        valorBoolean = ! (100 >= 10) && (10 < 20);
+        
+        // Operações com String
+        valorString = "Minha String";
+        valorString = valorString + " Texto ";
+        valorString = valorString + valorDouble;
+        System.out.println(valorString);
+        valorString = "Minha String";
+        
+        System.out.println(valorString);
+        System.out.println(valorString.substring(1, 8));
+        System.out.println(valorString.equalsIgnoreCase("minha string"));
+        
+        valorString.toUpperCase();
+        valorString.toLowerCase();
+        
+        System.out.println(valorString.contains("tring"));
+        System.out.println(valorString.startsWith("Minha"));
+        System.out.println(valorString.endsWith("Minha"));
+        System.out.println(valorString.replace("Minha", "Tua"));
+        
+        valorString = "   MINHA STRING    ";
+        System.out.println(valorString);
+        
+        System.out.println(valorString.trim());
+        System.out.println("Tamanho da String: " + valorString.length());
+        
+        // estruturas de Seleção
+        if (valorInteiro > 5) {
+            System.out.println("MAIOR QUE 5");
+        } else if (valorInteiro < 5) {
+            System.out.println("MENOR QUE 5");
+        } else {
+            System.out.println("IGUAL!");
+        }
+        
+        valorInteiro = 213;
+        
+        switch (valorInteiro) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("Sei la");
+                break;
+        }
     }
     
 }
