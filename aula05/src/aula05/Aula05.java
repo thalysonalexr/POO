27eb44ceccd01e6000026150563806d1d11d9319
Thalyson A. Rodrigues <thalysonrodrigues.dev@gmail.com>
2019-10-30 22:55:09 -0300

    aula05

--- aula05/src/aula05/Aula05.java
+++ aula05/src/aula05/Aula05.java
@@ -5,6 +5,8 @@
  */
 package aula05;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lab
@@ -16,6 +18,7 @@ public class Aula05 {
      */
     public static void main(String[] args) {
         
+        // estático
         Assunto[] assuntos = {
             new Assunto("Programação"),
             new Assunto("Arquitetura"),
@@ -24,38 +27,92 @@ public class Aula05 {
             new Assunto("Romance")
         };
         
-        Livro l1 = new Livro(2011, "Thalyson R.", "Programando com Python");
-        Livro l2 = new Livro(2012, "Thalyson R.", "Programando com PHP");
-        Livro l3 = new Livro(1999, "Thalyson R.", "Como programar: para iniciantes");
-        Livro l4 = new Livro(2000, "Thalyson R.", "Hello World!!!");
+        Livro[] livros = {
+            new Livro(2011, "Thalyson R.", "Programando com Python"),
+            new Livro(2012, "Thalyson R.", "Programando com PHP"),
+            new Livro(1999, "Thalyson R.", "Como programar: para iniciantes"),
+            new Livro(2000, "Thalyson R.", "Hello World!!!")
+        };
         
-        l1.addAssunto(assuntos[0]);
-        l1.addAssunto(assuntos[1]);
-        l1.addAssunto(assuntos[2]);
+        livros[0].addAssunto(assuntos[0]);
+        livros[0].addAssunto(assuntos[1]);
+        livros[0].addAssunto(assuntos[2]);
         
-        l2.addAssunto(assuntos[2]);
-        l2.addAssunto(assuntos[3]);
+        livros[1].addAssunto(assuntos[2]);
+        livros[1].addAssunto(assuntos[3]);
         
-        l3.addAssunto(assuntos[0]);
+        livros[2].addAssunto(assuntos[0]);
         
-        l4.addAssunto(assuntos[2]);
-        l4.addAssunto(assuntos[1]);
-        l4.addAssunto(assuntos[3]);
-        l4.addAssunto(assuntos[3]);
+        livros[3].addAssunto(assuntos[2]);
+        livros[3].addAssunto(assuntos[1]);
+        livros[3].addAssunto(assuntos[3]);
+        livros[3].addAssunto(assuntos[3]);
         
-        Emprestimo e1 = new Emprestimo("Alexandre T.", new ItemEmprestimo("25/02/2020", l1));
-        e1.addItemEmprestimo(new ItemEmprestimo("24/11/2019", l2));
-        e1.addItemEmprestimo(new ItemEmprestimo("04/12/2020", l3));
-        e1.addItemEmprestimo(new ItemEmprestimo("02/01/2020", l4));
+        Emprestimo e1 = new Emprestimo("Alexandre T.", new ItemEmprestimo("25/02/2020", livros[0]));
+        e1.addItemEmprestimo(new ItemEmprestimo("24/11/2019", livros[1]));
         
         Biblioteca biblioteca = new Biblioteca("Biblioteca Central");
         
-        biblioteca.novoLivro(l1);
-        
+        biblioteca.novoLivro(livros[0]);
+        biblioteca.novoLivro(livros[1]);
+        biblioteca.novoLivro(livros[2]);
+        biblioteca.novoLivro(livros[3]);
+
         biblioteca.novoEmprestimo(e1);
-        
+ 
         // biblioteca.imprimirLivros();
         biblioteca.imprimirEmprestimos();
+
+        // dinamico
+        /*
+        Scanner scan = new Scanner(System.in);
+        
+        String opcao = "";
+        
+        while (!(opcao.equals("x") || opcao.equals("X"))) {
+            System.out.print(Aula05.menu());
+            opcao = scan.nextLine();
+            
+            switch(opcao) {
+                case "1":
+                    biblioteca.novoLivro(Aula05.addLivro());
+                    break;
+            }
+        }
+        */
     }
     
+    private static String menu() {
+        return "1 - Inserir novo livro" +
+                "\nx - Sair" +
+                "\n>> ";
+    }
+    
+    private static Assunto getAssunto(int index) {
+        Assunto[] assuntos = {
+            new Assunto("Programação"),
+            new Assunto("Arquitetura"),
+            new Assunto("Matemática"),
+            new Assunto("Biologia"),
+            new Assunto("Romance")
+        };
+
+        return assuntos[index];
+    }
+    
+    private static Livro addLivro() {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Ano: ");
+        int ano = Integer.parseInt(scan.nextLine());
+        
+        System.out.print("Autor: ");
+        String autor = scan.nextLine();
+        
+        System.out.print("Nome: ");
+        String nome = scan.nextLine();
+        
+        return new Livro(ano, autor, nome);
+    }
 }
+
