517fd5e6feb200424181b728664f70863734c071
Thalyson A. Rodrigues <thalysonrodrigues.dev@gmail.com>
2019-11-13 20:37:33 -0300

    aula07

---
+++ aula07/src/aula07/FormaGeometrica.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula07;
+
+/**
+ *
+ * @author Lab
+ */
+public class FormaGeometrica {
+    
+    public double calcularArea() {
+        return 0;
+    }
+
+    public double calcularPerimetro() {
+        return 0;
+    }
+    
+    public String quemSou() {
+        return "FormaGeometrica";
+    }
+}
